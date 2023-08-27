0ef167ca493bcfb08e549da9a8d3668ba9ad24bd
Vishnu-01-cyber <vishnuchellathurai@gmail.com>
2023-08-27 22:23:33 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+
+
+export const data = [
+ 
+
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
+    title: "Music Player",
+    desc: "",
+    link: "",
+    git: "",
+  }
+  
+];
